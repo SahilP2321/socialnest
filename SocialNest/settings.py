1a1fd5ace553c4c6c677afce4bbb417a80f498f1
Sahil <patilsahil.23p@gmail.com>
2026-02-25 22:31:39 +0530

    Prepare for Render deployment

--- SocialNest/settings.py
+++ SocialNest/settings.py
@@ -17,16 +17,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nm%qdx19-%k+vi(tuunt!1htnf4%06qf1lhgv5d45)ws9@8bcb'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
